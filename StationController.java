d6c7d0d740ac97c9376a73a6965da42addbbac62
1088216 <1088216@students.adu.ac.ae>
2026-06-07 02:24:15 +0000

    feat: implement stations and zones API endpoint with Swagger docs

--- StationController.java
+++ StationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.alnaqel.system.dto.StationDto;
 
 import java.util.Arrays;
 import java.util.HashMap;
